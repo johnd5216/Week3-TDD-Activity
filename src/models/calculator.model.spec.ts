bbeb6e87a124808b296ca6fb6a734a537824e06a
johnd5216 <103228167+johnd5216@users.noreply.github.com>
2024-09-17 10:29:41 -0400

    Update calculator.model.spec.ts

--- src/models/calculator.model.spec.ts
+++ src/models/calculator.model.spec.ts
@@ -1,4 +1,6 @@
-
+import { ActionKeys } from '../enums/action-keys.enum';
+import { NumericKeys } from '../enums/numeric-keys.enum';
+import { OperatorKeys } from '../enums/operator-keys.enum';
 import { CalculatorModel } from './calculator.model';
 import { ICalculatorModel } from '../interfaces/calculator-model.interface';
 
@@ -11,5 +13,32 @@ describe('CalculatorModel', (): void => {
     expect(calculator).toBeDefined();
 
   });
+
+});
+
+it('should have an empty display on init', (): void => {
+
+  // Assemble
+  const calculator: ICalculatorModel = new CalculatorModel();
+
+  // Act
+  const displayValue: string = calculator.display();
+
+  // Assert
+  expect(displayValue).toEqual('');
+
+});
+
+it('should display `1` when the `1` key is pressed', (): void => {
+
+  // Assemble
+  const calculator: ICalculatorModel = new CalculatorModel();
+
+  // Act
+  calculator.pressNumericKey(NumericKeys.ONE);
+  const displayValue: string = calculator.display();
+
+  // Assert
+  expect(displayValue).toEqual('1');
 
 });
